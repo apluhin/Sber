b4ab7a1d6e400e8e06a06c28fed0f3a3ca9166fe
aleksejpluhin <pk-feso@mail.ru>
2016-07-28 08:31:08 +0300

    Задача 2056 и 2057

---
+++ src/less3/Solution.java
@@ -0,0 +1,57 @@
+package less3;
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+/**
+ * Created by aleksejpluhin on 27.07.16.
+ */
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        StringBuilder builder = new StringBuilder();
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        while (bufferedReader.ready()) {
+            builder.append(bufferedReader.readLine()).append(" ");
+        }
+
+
+        String line = builder.toString().toLowerCase();
+        String[] word = line.split(" ");
+        Map<String, Integer> map = new TreeMap<>();
+        for (String s : word) {
+            if (map.get(s) == null) {
+                map.put(s, 1);
+            } else {
+                map.put(s, map.get(s) + 1);
+            }
+        }
+        Set<Integer> set = new TreeSet<>(new Comparator<Integer>() {
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                return -1 * Integer.compare(o1,o2);
+            }
+        });
+        set.addAll(map.values());
+        set.remove(1);
+
+        for (Integer current : set) {
+            for (Map.Entry<String, Integer> stringIntegerEntry : map.entrySet()) {
+                if (current.equals(stringIntegerEntry.getValue())) {
+                    System.out.println(stringIntegerEntry.getKey().trim());
+                    stringIntegerEntry.setValue(-1);
+
+                }
+            }
+
+        }
+
+
+    }
+
+
+}
+
---
+++ src/less3/Solution1.java
@@ -0,0 +1,48 @@
+package less3;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+/**
+ * Created by aleksejpluhin on 28.07.16.
+ */
+public class Solution1 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(reader.readLine());
+        Map<Integer, Integer> map = new TreeMap<>();
+        while (n > 0) {
+            String[] arr = reader.readLine().split(" ");
+
+            if (arr.length == 1) {
+                System.out.println(min(map));
+            } else {
+                int num = Integer.parseInt(arr[1]);
+                if (map.get(num) != null) {
+                    map.put(num, map.get(num) + 1);
+                } else {
+                    map.put(num, 1);
+                }
+
+            }
+
+
+            n--;
+        }
+
+    }
+
+    public static int min(Map<Integer, Integer> map) {
+        for (Map.Entry<Integer, Integer> integerIntegerEntry : map.entrySet()) {
+            if (integerIntegerEntry.getValue() > 0) {
+                int value = integerIntegerEntry.getValue();
+                map.put(integerIntegerEntry.getKey(), value - 1);
+                return integerIntegerEntry.getKey();
+            }
+        }
+        return 0;
+    }
+}
