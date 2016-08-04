477bdf8805382793bf5b4fa3b18b27d5c9ce042e
aleksejpluhin <pk-feso@mail.ru>
2016-08-04 13:47:09 +0300

    home 4 правки

---
+++ src/less5/home/terminal/exception/TerminalException.java
@@ -0,0 +1,23 @@
+package less5.home.terminal.exception;
+
+public class TerminalException extends Exception {
+
+    public TerminalException() {
+    }
+
+    public TerminalException(String message) {
+        super(message);
+    }
+
+    public TerminalException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public TerminalException(Throwable cause) {
+        super(cause);
+    }
+
+    public TerminalException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+}
