39ec03079d780e4515e6faaf22727bd2ad76d259
aleksejpluhin <pk-feso@mail.ru>
2016-08-11 17:42:50 +0300

    home 7 define

--- src/main/java/less7/home2/encryptedloader/EncryptedClassLoader.java
+++ src/main/java/less7/home2/encryptedloader/EncryptedClassLoader.java
@@ -2,7 +2,6 @@ package less7.home2.encryptedloader;
 
 import java.io.File;
 import java.io.IOException;
-import java.lang.reflect.Method;
 
 public class EncryptedClassLoader extends ClassLoader {
 
@@ -22,13 +21,11 @@ public class EncryptedClassLoader extends ClassLoader {
         try {
 
             byte[] b = new Encrypter(new File(dir.getPath()), key).decript(name);
-            Method m = ClassLoader.class.getDeclaredMethod("defineClass", String.class, byte[].class, int.class, int.class);
-            m.setAccessible(true);
 
             return defineClass(name.substring(0, name.lastIndexOf(".")), b, 0, b.length);
 
 
-        } catch (IOException | NoSuchMethodException e) {
+        } catch (IOException e) {
             throw new RuntimeException("Error during load find class", e);
         }
 
