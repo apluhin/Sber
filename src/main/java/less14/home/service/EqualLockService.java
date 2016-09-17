0dc7168f6c6f328422060c08c1d82718d8d9a377
aleksejpluhin <pk-feso@mail.ru>
2016-09-17 22:40:36 +0300

    Try use method compute

--- src/main/java/less14/home/service/EqualLockService.java
+++ src/main/java/less14/home/service/EqualLockService.java
@@ -2,12 +2,13 @@ package less14.home.service;
 
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.atomic.AtomicIntegerArray;
+import java.util.concurrent.locks.ReentrantLock;
+import java.util.function.BiFunction;
 
 public class EqualLockService implements Service {
 
     private final Service service;
-    private final Map<Object, AtomicIntegerArray> map = new ConcurrentHashMap<>();
+    private final Map<Object, ReentrantLock> map1 = new ConcurrentHashMap<>();
 
 
     public EqualLockService(Service service) {
@@ -17,61 +18,46 @@ public class EqualLockService implements Service {
 
     @Override
     public void run(Object o) {
-
-        //Array Atomic for ordering in synchronization
-        map.putIfAbsent(o, new AtomicIntegerArray(new int[]{0, 0}));
-        Integer current;
-        current = getInteger(o);
-        service.run(o);
-        end(o, current);
-
-
-    }
-
-    private void end(Object o, Integer current) {
-        synchronized (map.get(o)) {
-            if (map.get(o).get(0) == current) {
-                System.out.println(o + " " + map.remove(o) + " remove");
-                return;
-            }
-            if (map.get(o).get(1) == 0) {
-                map.get(o).set(1, 2);
-            } else {
-                map.get(o).incrementAndGet(1);
-            }
-            map.get(o).notifyAll();
-        }
-    }
-
-    private Integer getInteger(Object o) {
-
+        map1.compute(o, getLock());
         try {
-            return tryGet(map.putIfAbsent(o, new AtomicIntegerArray(new int[]{0, 0})), o);
-        } catch (NullPointerException e) {
-            return tryGet(map.get(o), o);
+            service.run(o);
+        } finally {
+            map1.get(o).unlock();
+            map1.compute(o, remove());
         }
 
     }
 
-
-    public Integer tryGet(AtomicIntegerArray atomicIntegerArray, Object o) {
-        Integer current;
-        synchronized (atomicIntegerArray) {
-
-            current = map.get(o).incrementAndGet(0);
-            if (map.get(o).get(0) == 1) {
-                return 1;
+    private BiFunction<Object, ReentrantLock, ReentrantLock> getLock() {
+        BiFunction<Object, ReentrantLock, ReentrantLock> function = new BiFunction<Object, ReentrantLock, ReentrantLock>() {
+            @Override
+            public ReentrantLock apply(Object o, ReentrantLock reentrantLock) {
+                ReentrantLock lock = reentrantLock;
+                if (lock == null) lock = new ReentrantLock();
+                lock.lock();
+                return lock;
             }
-            while (map.get(o).get(1) != current) {
-                try {
-                    map.get(o).wait();
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
+        };
+        return function;
+    }
+
+    private BiFunction<Object, ReentrantLock, ReentrantLock> remove() {
+        BiFunction<Object, ReentrantLock, ReentrantLock> function = new BiFunction<Object, ReentrantLock, ReentrantLock>() {
+            @Override
+            public ReentrantLock apply(Object o, ReentrantLock reentrantLock) {
+                if (!(reentrantLock.isLocked() || reentrantLock.hasQueuedThreads())) {
+                    return null;
+                } else {
+                    return reentrantLock;
                 }
+
             }
-        }
-        return current;
+        };
+        return function;
     }
+
+
+
 }
 
 
